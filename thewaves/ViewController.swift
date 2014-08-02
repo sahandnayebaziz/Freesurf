eeafa6b20d9e26922516fa1866624e37dbe02d75
Sahand Nayebaziz <snayebaz@uci.edu>
2014-08-02 11:26:11 -0700

    Initial Commit

---
+++ thewaves/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thewaves
+//
+//  Created by Sahand Nayebaziz on 8/2/14.
+//  Copyright (c) 2014 Sahand Nayebaziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
