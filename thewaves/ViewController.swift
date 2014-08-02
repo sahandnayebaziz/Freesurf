3498cf3fa85bb86f1c7999393044b86aed2c366e
Sahand Nayebaziz <snayebaz@uci.edu>
2014-08-02 16:35:52 -0700

    the JSON is being shown on the display in its raw format
    - added a bare interface to see JSON coming in on the phone

--- thewaves/ViewController.swift
+++ thewaves/ViewController.swift
@@ -9,19 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController, NSURLSessionDelegate {
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
     let sourceURL:NSURL = NSURL(string: "http://api.spitcast.com/api/county/spots/orange-county/")
     var sourceSession:NSURLSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
     var sourceData:AnyObject?
     
+    @IBAction func clearData(sender: AnyObject) {
+        textArea.text = "Cleared."
+    }
+    @IBAction func loadData(sender: AnyObject) {
+        if !(textArea.text == "Cleared.") || !(textArea.text == "This is where the waves data will load once received.") {
+            let sourceTask = sourceSession.dataTaskWithURL(sourceURL, completionHandler: {(data, response, error) -> Void in
+                self.sourceData = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+                })
+            sourceTask.resume()
+            textArea.text = "\(self.sourceData)"
+            
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let sourceTask = sourceSession.dataTaskWithURL(sourceURL, completionHandler: {(data, response, error) -> Void in
-            self.sourceData = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
-            print(self.sourceData)
-        })
-        sourceTask.resume()
-
     }
 
     override func didReceiveMemoryWarning() {
