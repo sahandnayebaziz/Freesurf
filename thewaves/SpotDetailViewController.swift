a85f45f31584afe9fefd62423913f494c6daeb5d
Sahand Nayebaziz <snayebaz@uci.edu>
2014-12-14 14:07:03 -0800

    store floats instead of ints for tides and swell heights, add basic tide chart to detail view
    - storing ints instead of floats for tide values and swell heights before was fine because this information was only being displayed to the user in text, but seeing these values graphed, a lot of information was missing and the values resembled piece-wise functions instead of curves as they naturally are.
    - started using LineChart by Mirco Zeiss

--- thewaves/SpotDetailViewController.swift
+++ thewaves/SpotDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 // SpotDetailViewController displays detailed forecast information for a surf spot.
 // :: the user enters this view when they select a cell in the YourSpotsTableViewController
-class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+class SpotDetailViewController: UIViewController, UIScrollViewDelegate, LineChartDelegate {
     
     // spotLibrary is the SpotLibrary object containing spot and county data passed to this view controller from YourSpotsTableView
     var spotLibrary:SpotLibrary!
@@ -25,14 +25,23 @@ class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableVi
     // in YourSpotsTableView
     let gradient:CAGradientLayer = CAGradientLayer()
     
+    // the tideChart object
+    var tideChart:LineChart = LineChart()
+    
+    // this view is the view that gives tideChart it's size
+    @IBOutlet weak var copyView: UIView!
+    
+    // this view is the view that tideChart will be added to
+    @IBOutlet weak var targetView: UIView!
+    
+    // these labels display the tide that is selected by the user on the tide chart
+    @IBOutlet weak var tideChartTimeLabel: UILabel!
+    @IBOutlet weak var tideChartHeightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // add an edge swipe gesture recognizer to the left edge of the display
-        self.enterPanGesture = UIScreenEdgePanGestureRecognizer()
-        self.enterPanGesture.addTarget(self, action:"handleOnstagePan:")
-        self.enterPanGesture.edges = UIRectEdge.Left
-        self.view.addGestureRecognizer(self.enterPanGesture)
+        self.createEdgePanGestureRecognizer()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -41,6 +50,8 @@ class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         // set the background color of the view based on the size of this spot's swell height
         self.setBackgroundColor(self.spotLibrary.currentHeight(selectedSpotID)!)
         
+        // create the tide chart
+        self.createChartsForDetailView()
         
     }
 
@@ -48,75 +59,15 @@ class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
-    }
-
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    // MARK: Create and manage gesture recognizers
+    func createEdgePanGestureRecognizer() {
+        // add an edge swipe gesture recognizer to the left edge of the display
+        self.enterPanGesture = UIScreenEdgePanGestureRecognizer()
+        self.enterPanGesture.addTarget(self, action:"handleOnstagePan:")
+        self.enterPanGesture.edges = UIRectEdge.Left
+        self.view.addGestureRecognizer(self.enterPanGesture)
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func handleOnstagePan(pan: UIPanGestureRecognizer){
         // now lets deal with different states that the gesture recognizer sends
@@ -135,6 +86,7 @@ class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         }
     }
     
+    // MARK: Create and manage colors, labels, and display
     func setBackgroundColor(height:Int) {
         var colorTop:CGColorRef;
         var colorBottom:CGColorRef;
@@ -159,4 +111,241 @@ class SpotDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         return UIStatusBarStyle.LightContent
     }
 
+    
+    // MARK: Creating and managing charts
+    func createChartsForDetailView() {
+        
+        // set the frame for the chart
+        tideChart = LineChart(frame: CGRect(x: self.view.frame.minX, y: self.copyView.frame.minY, width: self.view.frame.width, height: self.copyView.frame.height))
+        
+        // add data to tide chart
+        var dataArray:Array<CGFloat> = []
+        var tides = self.spotLibrary.next24Tides(selectedSpotID)!
+        
+        for tide in tides {
+            dataArray.append(CGFloat(tide))
+        }
+        
+        tideChart.addLine(dataArray)
+        
+        // modify tide chart
+        let lightGray:UIColor = UIColor(red: 243/255.0, green: 243/255.0, blue: 243/255.0, alpha: 0.13)
+        
+        tideChart.animationEnabled = false
+        tideChart.areaUnderLinesVisible = true
+        tideChart.axesColor = lightGray
+        tideChart.gridColor = lightGray
+        
+        // add tideChart to the view
+        targetView.addSubview(tideChart)
+        
+        // add self as delegate
+        tideChart.delegate = self
+
+    }
+    
+    func didSelectDataPoint(x: CGFloat, yValues: Array<CGFloat>) {
+        tideChartHeightLabel.text = "\(Int(yValues.first!))ft"
+        
+        var timeStringIn12HourTime:String = ""
+        var hourOfDay = Int(x) + self.spotLibrary.currentHour
+        if hourOfDay >= 24 {
+            hourOfDay = hourOfDay - 24
+        }
+        if hourOfDay < 12 {
+            if hourOfDay == 0 {
+                timeStringIn12HourTime = "12AM"
+            }
+            else {
+                timeStringIn12HourTime = "\(hourOfDay)AM"
+            }
+        }
+        else if hourOfDay >= 12 {
+            hourOfDay = hourOfDay - 12
+            if hourOfDay == 0 {
+                timeStringIn12HourTime = "12PM"
+            }
+            else {
+                timeStringIn12HourTime = "\(hourOfDay)PM"
+            }
+        }
+        
+        tideChartTimeLabel.text = timeStringIn12HourTime
+    }
+
+    // MARK: View delegate methods
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        self.gradient.frame = self.view.bounds
+        self.setNeedsStatusBarAppearanceUpdate()
+        self.gradient.setNeedsDisplay()
+        tideChart.frame = CGRect(x: self.view.frame.minX, y: self.copyView.frame.minY, width: self.view.frame.width, height: self.copyView.frame.height)
+        tideChart.setNeedsDisplay()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    // MARK: - Table view data source
+//
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Potentially incomplete method implementation.
+//        // Return the number of sections.
+//        return 0
+//    }
+//
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete method implementation.
+//        // Return the number of rows in the section.
+//        return 0
+//    }
+//
+//
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+//
+//        // Configure the cell...
+//
+//        return cell
+//    }
+//
+//
+//    /*
+//    // Override to support conditional editing of the table view.
+//    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+//        // Return NO if you do not want the specified item to be editable.
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Override to support editing the table view.
+//    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//        if editingStyle == .Delete {
+//            // Delete the row from the data source
+//            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+//        } else if editingStyle == .Insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }
+//    }
+//    */
+//
+//    /*
+//    // Override to support rearranging the table view.
+//    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+//
+//    }
+//    */
+//
+//    /*
+//    // Override to support conditional rearranging of the table view.
+//    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+//        // Return NO if you do not want the item to be re-orderable.
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        // Get the new view controller using [segue destinationViewController].
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
