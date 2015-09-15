94fc8d327042b67ad4880e99c038355a305ae076
Sahand Nayebaziz <sahand@sahand.me>
2015-09-14 22:47:02 -0700

    create FSWatchKit target

---
+++ FSWatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  FSWatchKit Extension
+//
+//  Created by Sahand Nayebaziz on 9/14/15.
+//  Copyright © 2015 Sahand Nayebaziz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
